8fbe9b4f46b404dbde7fa16ee7a98965aeffc14a
Dung Vu <dung.vu@audiencemedia.com>
2016-04-25 10:50:46 +0700

    Initial Commit

---
+++ ObjectMapper+RealmTests/ObjectMapper_RealmTests.swift
@@ -0,0 +1,36 @@
+//
+//  ObjectMapper_RealmTests.swift
+//  ObjectMapper+RealmTests
+//
+//  Created by Dung Vu on 4/25/16.
+//  Copyright © 2016 Dung Vu. All rights reserved.
+//
+
+import XCTest
+@testable import ObjectMapper_Realm
+
+class ObjectMapper_RealmTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
